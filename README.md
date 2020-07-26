808d3ae020045ea210e60b38ec11c148168c1441
Michael Spengler <mycamper@gmx.de>
2020-07-26 23:23:17 +0200

    documentation improved

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Persistence
 
+With this module you can use GitHub as database. I guess this is only recommendable for small projects.
 
 
 ## Call usage example via command line
 ```
 deno run --allow-net --allow-read --allow-write --reload --allow-run https://raw.githubusercontent.com/michael-spengler/persistence/master/usage-example.ts
 
-deno run --allow-net --allow-read --allow-write --reload --allow-run usage-example.ts
 
 <!-- deno run --allow-net --allow-read --allow-write --reload --allow-run https://deno.land/x/persistence/usage-example.ts -->
 ```
@@ -14,8 +14,8 @@ deno run --allow-net --allow-read --allow-write --reload --allow-run usage-examp
 ## Execute the tests
 ```
 
-deno test --reload --allow-net --allow-run --allow-read --allow-write test.ts
 deno test --reload --allow-net --allow-run --allow-read --allow-write https://raw.githubusercontent.com/michael-spengler/persistence/master/test.ts
-deno test --reload --allow-net --allow-run --allow-read --allow-write https://deno.land/x/persistence/test.ts
 
-``` # persistence-example-project-folder
+
+<!-- deno test --reload --allow-net --allow-run --allow-read --allow-write https://deno.land/x/persistence/test.ts -->
+
--- usage-example.ts
+++ usage-example.ts
@@ -2,20 +2,25 @@
 // import { Persistence } from "https://deno.land/x/persistence/persistence.ts"
 import { Persistence } from "https://raw.githubusercontent.com/michael-spengler/persistence/master/persistence.ts"
 
-const projectPath = `${Deno.cwd()}/persistence-example-project-folder`
-const pathToFile = `${projectPath}/example-file.json`
+const projectPathForData = `${Deno.cwd()}/persistence-example-project-folder`
+
+
+
+/*************      Local Mode      *************/
+const pathToFile = `${projectPathForData}/example-file.json`
 await Persistence.saveToLocalFile(pathToFile, JSON.stringify([{foo: "foo :)"}]))
 
 const localFileContent = await Persistence.readFromLocalFile(pathToFile)
 console.log(localFileContent)
 
 
-// advanced mode :) 
+
+/*************      Remote Mode - using GitHub as database      *************/
 const userName = "michael-spengler"
 // import { password } from './topsecret/config.ts'
 const org = "michael-spengler"
 const repo = "persistence-example-project-folder"
-// await Persistence.commitAndPush(projectPath, userName, password, org, repo) // commented out as I should not publish my pw
+// await Persistence.commitAndPush(projectPathForData, userName, password, org, repo) // commented out as I should not publish my pw
 
 const linkToExampleDataRemote = "https://raw.githubusercontent.com/michael-spengler/persistence-example-project-folder/master/example-file.json"
 console.log(`remote file content: ${await Persistence.readFromRemoteFile(linkToExampleDataRemote)}`) 
