8db095fa82409ca78025af47bc9a4143a73ce251
Michael Spengler <mycamper@gmx.de>
2020-07-28 22:25:40 +0200

    logging

--- persistence.ts
+++ persistence.ts
@@ -7,25 +7,24 @@ export class Persistence {
 	private static encoder = new TextEncoder()
 
 	public static async saveToLocalFile(filePath: string, data: string) {
-
+		console.log(`saving data to local file: ${filePath}`)
 		await Deno.writeFile(`${filePath}`, Persistence.encoder.encode(data))
 	}
 
 	public static async readFromLocalFile(localFilePath: string): Promise<string> {
+		console.log(`reading data from local file: ${localFilePath}`)
 		return Persistence.decoder.decode(await Deno.readFile(localFilePath))
 	}
-
+	
 	public static async commitAndPush(projectPath: string, username: string, pw: string, org: string, repo: string, commitMessage: string = "automated commit"): Promise<void> {
 		let commandToBeExecuted = `./persistence-commit-and-push.sh ${projectPath} ${username} ${pw} ${org} ${repo} ${commitMessage} `
 		console.log(`executing command: ${commandToBeExecuted}`)
 		console.log(await CommandLineProcessor.process(commandToBeExecuted))
 	}
-
+	
 	public static async readFromRemoteFile(remoteFilePath: string) {
+		console.log(`reading data from remote file: ${remoteFilePath}`)
 		const apiResult = await api.get(remoteFilePath)
-		console.log('abc')
-		console.log(apiResult)
-		console.log('abc')
 
 		return JSON.stringify(apiResult)
 	}
