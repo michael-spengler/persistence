57ae30fec9071b0fb439757bed6efda335fdf9a7
Michael Spengler <mycamper@gmx.de>
2020-08-10 11:28:51 +0200

    replacing console.log ... by deno standard logger

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Persistence
 
 With this module you can use GitHub as a database. I guess this is only recommendable for small projects.
@@ -5,14 +6,20 @@ With this module you can use GitHub as a database. I guess this is only recommen
 
 ## Call usage example via command line
 ```
+  
 
 deno run --allow-net --allow-read --allow-write --allow-run https://deno.land/x/persistence@1.1.0/usage-example.ts
-
+  
+  
 ```
 
 If you get an errormessage like the one shown below, just:
 ```
+  
+
 mkdir persistence-example-project-folder 
+  
+
 ```
 as this folder is used for the usage example and for the tests.
 
@@ -21,7 +28,15 @@ as this folder is used for the usage example and for the tests.
 
 ## Execute the tests 
 ```
+  
 
 deno test --allow-net --allow-read --allow-write --allow-run https://deno.land/x/persistence@1.1.0/test.ts
+  
 
 ``` 
+
+## Support my Open Source Contributions  
+
+If you like my work please consider downloading the brave browser via my promotion link: [https://brave.com/fan464](https://brave.com/fan464).  
+
+![![](https://brave.com/)](https://brave.com/wp-content/uploads/2019/01/logotype-full-color.svg)
--- persistence.ts
+++ persistence.ts
@@ -1,3 +1,4 @@
+import * as log from "https://deno.land/std/log/mod.ts";
 import { CommandLineProcessor } from "https://deno.land/x/commandline_processor/commandline-processor.ts"
 import { Request } from 'https://deno.land/x/request@1.1.0/request.ts'
 
@@ -7,23 +8,23 @@ export class Persistence {
 	private static encoder = new TextEncoder()
 
 	public static async saveToLocalFile(filePath: string, data: string) {
-		console.log(`saving data to local file: ${filePath}`)
+		log.info(`saving data to local file: ${filePath}`)
 		await Deno.writeFile(`${filePath}`, Persistence.encoder.encode(data))
 	}
 
 	public static async readFromLocalFile(localFilePath: string): Promise<string> {
-		console.log(`reading data from local file: ${localFilePath}`)
+		log.info(`reading data from local file: ${localFilePath}`)
 		return Persistence.decoder.decode(await Deno.readFile(localFilePath))
 	}
 
 	public static async commitAndPush(projectPath: string, username: string, pw: string, org: string, repo: string, commitMessage: string = "automated commit"): Promise<void> {
 		let commandToBeExecuted = `./persistence-commit-and-push.sh ${projectPath} ${username} ${pw} ${org} ${repo} ${commitMessage} `
-		console.log(`executing command: ${commandToBeExecuted}`)
-		console.log(await CommandLineProcessor.process(commandToBeExecuted))
+		log.info(`executing command: ${commandToBeExecuted}`)
+		log.info(await CommandLineProcessor.process(commandToBeExecuted))
 	}
 
 	public static async readFromRemoteFile(remoteFilePath: string) {
-		console.log(`reading data from remote file: ${remoteFilePath}`)
+		log.info(`reading data from remote file: ${remoteFilePath}`)
 		const apiResult = await Request.get(remoteFilePath)
 
 		return JSON.stringify(apiResult)
